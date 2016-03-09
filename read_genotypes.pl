a66a616f168c0f2e842b1706f5471f04faa225d2
bellerbrock <bje24@cornell.edu>
2016-03-09 15:54:32 -0500

    commented out regular expression

--- read_genotypes.pl
+++ read_genotypes.pl
@@ -3,9 +3,7 @@
 use warnings;
 use strict;
 
-my $file = $ARGV[0];
-
-#my $file = 'genotypes.txt';
+my $file = $ARGV[0] || 'genotypes.txt';
 my $counter = 0;
 open(FILE,'<', $file) || die "Can't find file $file \n";
 open(OUT, '>', 'out.txt') || die "Can't open out file out.txt \n";
@@ -14,9 +12,11 @@ while (<FILE>) {
     chomp;
     $counter++;
     my($marker, $accession1, $accession2) = split('\t', $_);
-    print "marker = $marker \n";
-    $marker =~ s/^.{3}//;
-    print "marker = $marker \n";
+
+    #remove first 3 characters from marker string
+    #$marker =~ s/^.{3}//;
+
+   
     print OUT $marker . "\t" . $accession1 . "\t" . $accession2 ."   line number = $counter \n";  
     if ($counter == 3) { last;}
 }
